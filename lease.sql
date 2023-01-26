95e1c5d0b24f26bb3f92c1bfca370f7d66bcca0b
YiXiu <122330361+Tsaiyixiu@users.noreply.github.com>
2023-01-26 17:29:07 +0800

    Add files via upload

---
+++ lease.sql
@@ -0,0 +1,138 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2023-01-26 10:27:02
+-- 伺服器版本： 10.4.27-MariaDB
+-- PHP 版本： 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `lease`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `customers`
+--
+
+CREATE TABLE `customers` (
+  `CustomersID` varchar(5) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `CustomersName` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `ContactName` varchar(30) DEFAULT NULL,
+  `ContactTitle` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `Address` varchar(60) DEFAULT NULL,
+  `City` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `Region` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `PostalCode` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `Country` varchar(15) DEFAULT NULL,
+  `Phone` varchar(24) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `Fax` varchar(24) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `employess`
+--
+
+CREATE TABLE `employess` (
+  `EmployeeID` int(11) NOT NULL,
+  `LastName` varchar(20) NOT NULL,
+  `FirstName` varchar(10) NOT NULL,
+  `BirthDate` datetime DEFAULT NULL,
+  `HireDate` datetime DEFAULT NULL,
+  `Address` varchar(60) DEFAULT NULL,
+  `City` varchar(15) DEFAULT NULL,
+  `Region` varchar(15) DEFAULT NULL,
+  `PostalCode` varchar(10) DEFAULT NULL,
+  `Country` varchar(15) DEFAULT NULL,
+  `HomePhone` varchar(24) DEFAULT NULL,
+  `Extension` varchar(4) DEFAULT NULL,
+  `Photo` longblob DEFAULT NULL,
+  `Notes` mediumtext NOT NULL,
+  `ReportsTo` int(11) DEFAULT NULL,
+  `PhotoPath` varchar(255) DEFAULT NULL,
+  `Salary` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `order details`
+--
+
+CREATE TABLE `order details` (
+  `OrderID` int(11) NOT NULL,
+  `ProductID` int(11) NOT NULL,
+  `UnitPrice` int(11) NOT NULL,
+  `Quantity` smallint(2) NOT NULL,
+  `Discount` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `orders`
+--
+
+CREATE TABLE `orders` (
+  `OrderID` int(11) NOT NULL,
+  `CustomerID` varchar(5) DEFAULT NULL,
+  `EmployeeID` int(11) DEFAULT NULL,
+  `OrderDate` datetime DEFAULT NULL,
+  `RequiredDate` datetime DEFAULT NULL,
+  `ShippedDate` datetime DEFAULT NULL,
+  `ShipVia` int(11) DEFAULT NULL,
+  `Freight` int(11) NOT NULL,
+  `ShipAddress` int(11) DEFAULT NULL,
+  `City` int(11) DEFAULT NULL,
+  `Region` int(11) DEFAULT NULL,
+  `PostalCode` int(11) DEFAULT NULL,
+  `Country` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `products`
+--
+
+CREATE TABLE `products` (
+  `ProductID` int(11) NOT NULL,
+  `ProductName` varchar(40) NOT NULL,
+  `SupplierID` int(11) DEFAULT NULL,
+  `CategoryID` int(11) DEFAULT NULL,
+  `QuantityPerUnit` varchar(20) DEFAULT NULL,
+  `UnitPrice` int(11) NOT NULL,
+  `UnitsInStock` smallint(2) NOT NULL,
+  `UnitsOnOrder` smallint(2) NOT NULL,
+  `Discontinued` bit(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `region`
+--
+
+CREATE TABLE `region` (
+  `RegionID` int(11) NOT NULL,
+  `RegionDescription` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
